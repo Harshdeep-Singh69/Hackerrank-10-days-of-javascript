cb683f1d24a0c849b5bcb50dc982fd6b61c650bf
Harshdeep Singh <harshdeeps269@gmail.com>
2021-07-11 16:24:07 +0530

    Create Arithmetic Operators.js

---
+++ Arithmetic Operators.js
@@ -0,0 +1,32 @@
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    
+    area = length * width ;
+    // Write your code here
+    
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    perimeter= 2*(length + width);
+    // Write your code here
+    
+    return perimeter;
+}
